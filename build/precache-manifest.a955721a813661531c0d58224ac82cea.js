df07542d9bf95abc601c331f0cdc5290f637d350
deepak <techwinner20@gmail.com>
2020-06-06 23:15:56 +0530

    change

---
+++ build/precache-manifest.a955721a813661531c0d58224ac82cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ca486a09171d28746c0e00339fd673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6617d41b9c0264da47",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41109eccd8e206715723",
+    "url": "/static/js/2.07df8023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07df8023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6617d41b9c0264da47",
+    "url": "/static/js/main.97727ba3.chunk.js"
+  },
+  {
+    "revision": "07933a0154b217ac3b35",
+    "url": "/static/js/runtime-main.d91f79ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
